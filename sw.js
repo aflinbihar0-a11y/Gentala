0bf7102f09f36bb48b4c0deb942efca8469bd620
Aflin Bihar <aflinbihar0@gmail.com>
2026-08-03 22:23:06 +0800

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'gentala-offline-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './sw.js'
+];
+
+// Install Event: Cache halaman dan file pendukung
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event: Bersihkan cache lama
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event: Buka dari Cache jika offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
