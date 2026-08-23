ed8edc3312c931de4816a853fc244224a5237b09
Aflin Bihar <aflinbihar0@gmail.com>
2026-08-23 22:03:23 +0800

    Update 2_Skrining_Gizi.py

--- pages/2_Skrining_Gizi.py
+++ pages/2_Skrining_Gizi.py
@@ -51,7 +51,7 @@ def plot_kurva_zscore(df_ref, x_col, x_val, y_val, title_text, x_label, y_label)
         ),
         yaxis=dict(
             title=y_label,
-            dtick=10,         # Sumbu Y meloncat kelipatan 10
+            dtick=4,         # Sumbu Y meloncat kelipatan 4
             showgrid=True
         ),
         legend=dict(orientation="h", yanchor="bottom", y=-0.3, xanchor="center", x=0.5),
